45989ba2428d51083ade73c192093bce5b1e8231
keiiixd <117779265+keiiixd@users.noreply.github.com>
2022-11-08 16:29:07 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+const sisoyBtn = document.querySelector('#sisoyBtn');
+
+sisoyBtn.addEventListener('click', function() {
+    alert('great, congratulations')
+});
+
+const noBtn = document.querySelector('#noBtn');
+
+noBtn.addEventListener('click', function() {
+    alert('do you need to do your homework uh, so please do it')
+});
